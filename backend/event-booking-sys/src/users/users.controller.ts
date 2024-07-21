1743fcd0cecbe88ac43cd7eaaad128fe7b00f4ed
amaankhan4 <ak7165359@gmail.com>
2024-07-21 23:00:56 +0530

    edc

---
+++ backend/event-booking-sys/src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma } from '@prisma/client';
+import { UserLoginDto,UserSignUpDto,UserUpdateDto } from './dto/user-dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: Prisma.userCreateInput) {
+    return this.usersService.create(createUserDto);
+  }
+
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: Prisma.userUpdateInput) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ backend/event-booking-sys/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
+import { UserLoginDto,UserSignUpDto,UserUpdateDto } from './dto/user-dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly databaseService : DatabaseService){}
+  create(createUserDto: Prisma.userCreateInput) {
+    return this.databaseService.user.create({
+      data:createUserDto
+    })
+  }
+
+
+  update(id: number, updateUserDto: Prisma.userUpdateInput) {
+    return this.databaseService.user.update({
+      where:{
+        userid_ :id,
+      },
+      data:updateUserDto
+    })
+  }
+
+  remove(id: number) {
+    return this.databaseService.user.delete({
+      where:{
+        userid_:id
+      }
+    })
+  }
+}
